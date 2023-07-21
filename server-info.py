b6f75982ad2c10a9c60a1b99c3ad1b56a68387a6
freddy gbaguidi <gbaguidi.freddy@yahoo.com>
2023-07-21 10:16:33 +0200

    Version v0.1.1-linux2 - Correction de la valeur incorrecte

--- server-info.py
+++ server-info.py
@@ -10,5 +10,5 @@ Type "help", "copyright", "credits" or "license()" for more information.
 ... # Print the memory usage details
 ... print("Total Memory: {:.2f} GB".format(memory.total / (1024 ** 3)))
 ... print("Available Memory: {:.2f} GB".format(memory.available / (1024 ** 3)))
-... print("Used Memory: {:.2f} GB".format(memory.available / (1024 ** 3)))
+... print("Used Memory: {:.2f} GB".format(memory.used / (1024 ** 3)))
 ... print("Memory Percent: {:.2f}%".format(memory.percent))
